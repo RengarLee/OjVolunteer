c49273d47206f37e23118cebfa14a39222cf72a5
RengarLee <RengarLee@163.com>
2018-04-01 12:13:49 +0800

    Login页面添加

---
+++ OjVolunteer.UIPortal/Content/oj/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    var topback = $('#topback');
+    // 返回顶部按钮点击事件
+    topback.on("click",function(){
+        $('html,body').animate({
+        	scrollTop:0
+        },200)
+
+    })
+    $(window).on('scroll',function(){
+    	if($(window).scrollTop()>200)
+    		topback.fadeIn();
+    	else
+    		topback.fadeOut();
+    })
+
+    $(window).trigger('scroll');
+
+})
