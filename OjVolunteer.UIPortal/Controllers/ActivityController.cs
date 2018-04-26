43c1d033aa5fdc57590a7129f03510157c83993a
RengarLee <RengarLee@163.com>
2018-04-26 19:45:35 +0800

    活动审核完成

--- OjVolunteer.UIPortal/Controllers/ActivityController.cs
+++ OjVolunteer.UIPortal/Controllers/ActivityController.cs
@@ -12,6 +12,7 @@ namespace OjVolunteer.UIPortal.Controllers
     {
         short delNormal = (short)Model.Enum.DelFlagEnum.Normal;
         short delDelete = (short)Model.Enum.DelFlagEnum.Deleted;
+        short delAuditing = (short)Model.Enum.DelFlagEnum.Auditing;
         public IActivityTypeService ActivityTypeService { get; set; }
         public IActivityService ActivityService { get; set; }
         public IOrganizeInfoService OrganizeInfoService { get; set; }
@@ -23,11 +24,36 @@ namespace OjVolunteer.UIPortal.Controllers
             return View();
         }
 
+        #region Query
+        [ActionAuthentication(AbleOrganize = true, AbleUser = false)]
+        public ActionResult ActivityOfAuditing()
+        {
+            return View(LoginOrganize);
+        }
+
+        [ActionAuthentication(AbleOrganize = true, AbleUser = false)]
+        public ActionResult GetAllActivityOfAuditing()
+        {
+            var s = Request["limit"];
+            int pageSize = int.Parse(Request["limit"] ?? "5");
+            int offset = int.Parse(Request["offset"] ?? "0");
+            int pageIndex = (offset / pageSize) + 1;
+            var pageData = ActivityService.GetPageEntities(pageSize, pageIndex, out int total, o => o.Status == delAuditing, u => u.ActivityID, true).Select(u => new { u.ActivityID, u.ActivityName, u.ApplyUserInfo.UserInfoShowName, u.ApplyOrganizeInfo.OrganizeInfoShowName, u.ActivityPrediNum,u.ActivityType.ActivityTypeName,u.CreateTime,u.Status ,u.ActivityManagerID}).AsQueryable();
+            if (LoginOrganize.OrganizeInfoManageId != null)
+            {
+                pageData = pageData.Where(u => u.ActivityManagerID == LoginOrganize.OrganizeInfoID).AsQueryable();
+            }
+            var data = new { total = pageData.Count(), rows = pageData.ToList() };
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion
+
         #region Create
         /// <summary>
-        /// 组织进入活动申请界面
-        /// </summary>
-        /// <returns></returns>
+            /// 组织进入活动申请界面
+            /// </summary>
+            /// <returns></returns>
         [ActionAuthentication(AbleOrganize = true)]
         public ActionResult OrgCreate()
         {
@@ -66,6 +92,69 @@ namespace OjVolunteer.UIPortal.Controllers
             }
             return Content("fail");
         }
+        #endregion
+
+        #region Edit
+
+        /// <summary>
+        /// 批量同意活动申请
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        [ActionAuthentication(AbleOrganize = true)]
+        public ActionResult EditOfList(string ids)
+        {
+            if (string.IsNullOrEmpty(ids))
+            {
+                return Content("null");
+            }
+            string[] strIds = Request["ids"].Split(',');
+            List<int> idList = new List<int>();
+            foreach (var strId in strIds)
+            {
+                idList.Add(int.Parse(strId));
+            }
+            if (ActivityService.NormalListByULS(idList))
+            {
+                return Content("success");
+            }
+            else
+            {
+                return Content("fail");
+            }
+        }
+
+        #endregion
+
+        #region Delete
+
+        /// <summary>
+        /// 批量删除申请的活动
+        /// </summary>
+        /// <param name="ids"></param>
+        /// <returns></returns>
+        public ActionResult DeleteOfList(string ids)
+        {
+            if (string.IsNullOrEmpty(ids))
+            {
+                return Content("null");
+            }
+            string[] strIds = Request["ids"].Split(',');
+            List<int> idList = new List<int>();
+            foreach (var strId in strIds)
+            {
+                idList.Add(int.Parse(strId));
+            }
+            if (ActivityService.InvalidListByULS(idList))
+            {
+                return Content("success");
+            }
+            else
+            {
+                return Content("fail");
+            }
+        }
+
         #endregion
 
     }
