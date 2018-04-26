fa39c017cf4b3cd465bc48639d105d4406fc90e4
RengarLee <RengarLee@163.com>
2018-04-26 10:33:33 +0800

    AcitivtyType CRUD Test 完成

--- OjVolunteer.UIPortal/Controllers/ActivityTypeController.cs
+++ OjVolunteer.UIPortal/Controllers/ActivityTypeController.cs
@@ -1,5 +1,6 @@
 ﻿using OjVolunteer.IBLL;
 using OjVolunteer.Model;
+using OjVolunteer.UIPortal.Filters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,73 +9,102 @@ using System.Web.Mvc;
 
 namespace OjVolunteer.UIPortal.Controllers
 {
-    public class ActivityTypeController : Controller
+    public class ActivityTypeController : BaseController
     {
         short delNormal = (short)Model.Enum.DelFlagEnum.Normal;
         public IActivityTypeService ActivityTypeService { get; set; }
-        // GET: ActivityType
+
+        [ActionAuthentication(AbleOrganize = true, Super = true)]
         public ActionResult Index()
         {
-            return View();
+            return View(LoginOrganize);
         }
 
-        #region  加载所有活动类型 
+        #region Query
+
+        [HttpPost]
+        [ActionAuthentication(AbleOrganize = true, Super = true)]
         public ActionResult GetAllActivityType()
         {
-            //TODO:分页使用  BS Table
-            return View();
+            var s = Request["limit"];
+            int pageSize = int.Parse(Request["limit"] ?? "5");
+            int offset = int.Parse(Request["offset"] ?? "0");
+            int pageIndex = (offset / pageSize) + 1;
+            var pageData = ActivityTypeService.GetPageEntities(pageSize, pageIndex, out int total, o => o.Status == delNormal, u => u.ActivityTypeID, true).Select(u => new { u.ActivityTypeName, u.ModfiedOn, u.ActivityTypeID }).ToList();
+            var data = new { total = total, rows = pageData };
+            return Json(data, JsonRequestBehavior.AllowGet);
         }
         #endregion
 
-        #region Add
-        public ActionResult Add()
-        {
-            //TODO:打开添加对话框
-            return View();
-        }
+        #region Create
 
         [HttpPost]
-        public ActionResult Add(ActivityType activityType)
+        [ActionAuthentication(AbleOrganize = true, Super = true)]
+        public ActionResult Create(ActivityType activityType)
         {
-            //TODO:Test
-            activityType.CreateTime = DateTime.Now;
-            activityType.ModfiedOn = DateTime.Now;
-            activityType.Status = delNormal;
-            return Content("ok");
+            if (ModelState.IsValid)
+            {
+                if (ActivityTypeService.GetEntities(m => m.ActivityTypeName.Equals(activityType.ActivityTypeName)&&m.Status == delNormal).Count() > 0)
+                {
+                    return Content("exist");
+                }
+                activityType.CreateTime = DateTime.Now;
+                activityType.ModfiedOn = DateTime.Now;
+                activityType.Status = delNormal;
+                if (ActivityTypeService.Add(activityType) != null)
+                {
+                    return Content("success");
+                }
+            }
+            return Content("fail");
+
         }
         #endregion
 
         #region Edit
-        public ActionResult Edit(int id)
-        {
-            //TODO:加载编辑对话框
-            ActivityType activityType = ActivityTypeService.GetEntities(p => p.ActivityTypeID == id && p.Status == delNormal).FirstOrDefault();
-            return View(activityType);
-        }
 
+        /// <summary>
+        /// 行编辑活动类型名称
+        /// </summary>
+        /// <param name="activityType"></param>
+        /// <returns></returns>
         [HttpPost]
+        [ActionAuthentication(AbleOrganize = true, Super = true)]
         public ActionResult Edit(ActivityType activityType)
         {
-            //TODO:Test
-            string result = String.Empty;
-            if (ActivityTypeService.Update(activityType))
+            if (ModelState.IsValid)
             {
-                result = "ok";
+                string result = String.Empty;
+                activityType.ModfiedOn = DateTime.Now;
+                ActivityType temp = ActivityTypeService.GetEntities(d => d.ActivityTypeName.Equals(activityType.ActivityTypeName) && d.Status == delNormal).FirstOrDefault();
+                if (temp != null)
+                {
+                    result = ("exist");
+                }
+                else
+                {
+                    if (ActivityTypeService.Update(activityType))
+                    {
+                        result = "success";
+                    }
+                    else
+                    {
+                        result = "fail";
+                    }
+                }
+                return Json(new { result = result }, JsonRequestBehavior.AllowGet);
             }
-            else
-            {
-                result = "error";
-            }
-            return Content(result);
+            return Json(new { result = "fail" }, JsonRequestBehavior.AllowGet);
         }
         #endregion
 
         #region Delete
-        public ActionResult Delete(string ids)
+        [ActionAuthentication(AbleOrganize = true, Super = true)]
+        public ActionResult DeleteOfList(string ids)
         {
             if (string.IsNullOrEmpty(ids))
             {
-                return Content("Please Select!");
+                return Content("null");
             }
             string[] strIds = Request["ids"].Split(',');
             List<int> idList = new List<int>();
@@ -82,17 +112,14 @@ namespace OjVolunteer.UIPortal.Controllers
             {
                 idList.Add(int.Parse(strId));
             }
-            //批量删除
-            #region 逻辑删除
             if (ActivityTypeService.DeleteListByLogical(idList) > 0)
             {
-                return Content("error");
+                return Content("success");
             }
             else
             {
-                return Content("ok");
+                return Content("fail");
             }
-            #endregion
         }
         #endregion
     }
