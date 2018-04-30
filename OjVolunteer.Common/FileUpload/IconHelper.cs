20ced4d84a867e2a5e81a1b6cb5f823e6108d23b
RengarLee <RengarLee@163.com>
2018-04-30 12:02:31 +0800

    头像上传完成 未测试

---
+++ OjVolunteer.Common/FileUpload/IconHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace OjVolunteer.Common.FileUpload
+{
+    public class FileHelper
+    {
+        public static string IconUpload(HttpPostedFileBase file)
+        {
+            try
+            {
+                string path = System.Configuration.ConfigurationManager.AppSettings["DefaultIconSavePath"] + DateTime.Now.Year + "/" + DateTime.Now.Month + "/";
+                string dirPath = Path.GetFullPath(path);
+                if (!Directory.Exists(dirPath))
+                {
+                    Directory.CreateDirectory(dirPath);
+                }
+                string fileName = path + Guid.NewGuid().ToString().Substring(1, 5) + "-" + file.FileName;
+                file.SaveAs(Path.GetFullPath(fileName));
+                return fileName;
+            }
+            catch (Exception e)
+            {
+                return "";
+            }
+        }
+    }
+}
