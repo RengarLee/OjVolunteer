596df074284a1181dd6cd5f69df8b84d5cc46181
RengarLee <RengarLee@163.com>
2018-06-06 18:39:04 +0800

    排行榜

--- OjVolunteer.Model/ViewModel/TopView.cs
+++ OjVolunteer.Model/ViewModel/TopView.cs
@@ -10,7 +10,8 @@ namespace OjVolunteer.Model.ViewModel
     public class TopView
     {
         public int UserInfoID { get; set; }
-        public string UserInfoShowName { get; set; }
+        public string ShowName { get; set; }
         public decimal ActivityTime { get; set; }
+        public int OrgId { get; set; }
     }
 }
