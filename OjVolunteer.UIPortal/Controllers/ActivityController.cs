ca4726322ed55df04207f76c7bd229e880d1bdce
RengarLee <RengarLee@163.com>
2018-06-24 12:44:09 +0800

    筛选条件

--- OjVolunteer.UIPortal/Controllers/ActivityController.cs
+++ OjVolunteer.UIPortal/Controllers/ActivityController.cs
@@ -540,9 +540,8 @@ namespace OjVolunteer.UIPortal.Controllers
 
             if (LoginUser != null)
             {
-                DataPage = DataPage.Where(u => !u.ActivityMajor.Contains("," + LoginUser.MajorID + ",") && !u.ActivityPolitical.Contains("," + LoginUser.PoliticalID + ",") && !u.ActivityDepartment.Contains("," + LoginUser.DepartmentID + ",")).AsQueryable();
+                DataPage = DataPage.Where(u => u.ActivityManagerID == LoginUser.UserInfoID||(!u.ActivityMajor.Contains("," + LoginUser.MajorID + ",") && !u.ActivityPolitical.Contains("," + LoginUser.PoliticalID + ",") && !u.ActivityDepartment.Contains("," + LoginUser.DepartmentID + ","))).AsQueryable();
             }
-            //DataPage = DataPage.Where(u => u.ActivityEnrollStart < DateTime.Now).AsQueryable();
             return DataPage;
         }
         #endregion
